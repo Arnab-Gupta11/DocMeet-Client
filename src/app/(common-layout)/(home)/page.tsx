377bc7d9beb69f3b363799ed2203423ce268fc23
Arnab Gupta <arnabgupta1111@gmail.com>
2025-06-11 22:31:13 +0600

    feat(ui): add ThemeToggler component for light/dark mode.

---
+++ src/app/(common-layout)/(home)/page.tsx
@@ -0,0 +1,13 @@
+import { ThemeToggler } from "@/components/shared/ThemeToggler";
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      HomePage
+      <ThemeToggler />
+    </div>
+  );
+};
+
+export default HomePage;
--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="h-screen w-full bg-primary-bg-dark p-20">
-      <span className="mt-20 text-5xl font-bold gradient-text ">DocMeet</span>
-    </div>
-  );
-}
